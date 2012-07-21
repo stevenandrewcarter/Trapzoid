c710f54358fd28452151a2b030326e252e7eeef8
Steven <stevenc@voxtelecom.co.za>
2012-07-21 11:04:00 +0200

    Initial Commit
    Basic project creation

---
+++ Trapzoid.Agent/Engine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trapzoid.Agent {
+  /// <summary>
+  /// Calculates the state and makes the choice for the next move
+  /// </summary>
+  public class Engine {
+  }
+}
---
+++ Trapzoid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trapzoid {
+  class Program {
+    static void Main(string[] args) {
+      // Read the state of the world
+    }
+  }
+}
